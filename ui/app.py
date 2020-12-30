95f536e41191d09a34b66e7e94932129c4d32474
Anna <annahakhverdyan98@gmail.com>
2020-12-31 00:45:53 +0400

    Full functional UI is ready to be used

--- ui/app.py
+++ ui/app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from funcs.functions import *
 # import .utils
 
 app = Flask(__name__)
 
+FILE_NAMES = None
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
+        data = random_data()
+        global FILE_NAMES
+        FILE_NAMES = [data['episode_1_name'], data['episode_2_name']]
+
+        convert_episodes(data)
         return render_template("index.html")
 
     elif request.method == 'POST':
-        value = submit(request.form)
-        print(value)
-        return render_template("index.html")
+        if FILE_NAMES is not None:
+            value = submit(request.form)
+            save_csv(FILE_NAMES + [value])
 
-
+        return redirect('/')
 
 
 if __name__ == "__main__":
--- ui/funcs/functions.py
+++ ui/funcs/functions.py
@@ -1,9 +1,71 @@
+import random
+import pickle
+from os import listdir
+from os.path import isfile, join
+from cv2 import VideoWriter, VideoWriter_fourcc
+import torch
+import numpy as np
+import imageio
+import csv
+
 def submit(form):
     if form.get("btn1"):
-       return [1, 0]
+        return [1, 0]
     elif form.get("btn2"):
-       return [0, 1]
+        return [0, 1]
     elif form.get("btn3"):
         return [0.5, 0.5]
     else:
-        return None
+        return [0, 0]
+
+
+def unpickle(path):
+    with open(path, 'rb') as fin:
+        data = pickle.load(fin)
+    return data
+
+
+def select_random_data(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    rand_file_names = random.sample(files, 2)
+    values = list()
+
+    for file_name in rand_file_names:
+        values.append(unpickle(path + file_name))
+        values.append(file_name)
+
+    return values
+
+
+def random_data(path='./data/reward/'):
+    episodes_data = select_random_data(path)
+    episode_1, name_1, episode_2, name_2 = episodes_data
+    images_1, images_2 = episode_1['images'], episode_2['images']
+
+    return {
+        'episode_1': np.array(torch.stack(images_1), dtype=np.uint8),
+        'episode_1_name': name_1,
+        'episode_2': np.array(torch.stack(images_2), dtype=np.uint8),
+        'episode_2_name': name_2,
+    }
+
+
+def make_video(images, name):
+    # have a folder of output where output files could be stored.
+    write_to = '{}.mp4'.format(name)
+    writer = imageio.get_writer(write_to, format='mp4', mode='I', fps=30)
+
+    for im in images:
+        writer.append_data(im.T)
+    writer.close()
+
+
+def convert_episodes(data, path='./ui/static/videos/'):
+    for i in range(1, 3):
+        name = 'episode_' + str(i)
+        make_video(data[name], path + name)
+
+def save_csv(row, path='./data/target.csv'):
+    with open(path,'a', newline='') as f:
+          writer = csv.writer(f)
+          writer.writerow(row)
